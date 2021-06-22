262c46d2cbcf8e4c2b4511f7529d34fbadd98aaf
Serah wakaba <serahwakaba9@gmail.com>
2021-06-22 12:26:44 +0300

    tudents_instantiatesWithStudentId correctly

--- src/test/java/models/StudentProfileTest.java
+++ src/test/java/models/StudentProfileTest.java
@@ -13,6 +13,18 @@ class StudentProfileTest {
         assertEquals(true,testStudent instanceof StudentProfile);
     }
 
+    @Test
+    public void students_instantiatesWithName_String() throws Exception {
+        StudentProfile testStudent = createStudent();
+        assertEquals("Serah", testStudent.getName());
+    }
+
+    @Test
+    public void students_instantiatesWithStudentId_int() throws Exception {
+        StudentProfile testStudent = createStudent();
+        assertEquals(1,testStudent.getId());
+    }
+
     public StudentProfile createStudent()
     {
         return new StudentProfile("Serah",1,"saragathoni888@gmail.com","Software Development");
