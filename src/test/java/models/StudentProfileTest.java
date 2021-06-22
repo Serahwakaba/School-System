af1975e415a1ad75a434237e49a19ddce7f91d92
Serah wakaba <serahwakaba9@gmail.com>
2021-06-22 13:06:38 +0300

    studentprofile tests commit

--- src/test/java/models/StudentProfileTest.java
+++ src/test/java/models/StudentProfileTest.java
@@ -24,10 +24,83 @@ class StudentProfileTest {
         StudentProfile testStudent = createStudent();
         assertEquals(1,testStudent.getId());
     }
+    @Test
+    public void equals_returnsTrueIfNameAndStudentIddAreSame_true() {
+        StudentProfile testStudent = createStudent();
+        StudentProfile AnotherStudent = createStudent();
+        assertTrue(testStudent.equals(anotherStudent));
+    }
+    @Test
+    public void save_savesStudentToTheDatabase() throws Exception {
+        StudentProfile testStudent = createStudent();
+        testStudent.save();
+        assertTrue(StudentProfile.getAll().get(0).equals(testStudent));
+    }
 
-    public StudentProfile createStudent()
+    @Test
+    public void save_assignsIdToStudent() {
+        StudentProfile testStudent = createStudent();
+        testStudent.save();
+        StudentProfile savedStudent = StudentProfile.getAll().get(0);
+        assertEquals(savedStudent.getId(), testStudent.getId());
+    }
+
+    @Test
+    public void getAll_returnsAllInstancesOfStudent() {
+        StudentProfile testStudent = createStudent();
+        firstStudent.save();
+        StudentProfile secondStudent = createSecondStudent();
+        secondStudent.save();
+        assertEquals(true,StudentProfile.getAll().get(0).equals(firstStudent));
+        assertEquals(true,StudentProfile.getAll().get(1).equals(secondStudent));
+    }
+
+    @Test
+    public void find_returnsAnimalWithSameId_secondAnimal() {
+        StudentProfile firstStudent = createStudent();
+        firstStudent.save();
+        StudentProfile secondAnimal = createSecondStudent();
+        secondAnimal.save();
+        assertEquals( StudentProfile.findById(secondAnimal.getId()), secondAnimal);
+    }
+
+    @Test
+    public void update_updatesAnimalDetails() throws Exception {
+        StudentProfile testStudent=createStudent();
+        testStudent.save();
+
+        testStudent.update("Buyahi",2,"4456","mobile dev");
+        StudentProfile updatedAnimal=StudentProfile.findById(testStudent.getId());
+        assertEquals(testStudent.getId(),updatedAnimal.getId());
+        assertNotEquals(testStudent.getName(),updatedAnimal.getName());
+        assertNotEquals(testStudent.getId(),updatedAnimal.getId());
+        assertNotEquals(testStudent.getAdmissionNumber(),updatedAnimal.getAdmissionNumber());
+        assertNotEquals(testStudent.getEmail(),updatedAnimal.getEmail());
+    }
+
+    @Test
+    public void removeById_removesAnimalById() {
+        StudentProfile testAnimals = createStudent();
+        testAnimals.save();
+
+        testAnimals.removeById();
+        assertEquals(null,StudentProfile.findById(testAnimals.getId()));
+    }
+
+    @Test
+    public void removeAll_clearsAnimalDatabase() {
+        StudentProfile testAnimal = createStudent();
+        testAnimal.save();
+        testAnimal.removeAll();
+        assertEquals(null,  StudentProfile.findById(testAnimal.getId()));
+    }
+
+
+    public StudentProfile createStudent() { return new StudentProfile("Serah",1,"saragathoni888@gmail.com","Software Development");
+    }
+    public StudentProfile createSecondStudent()
     {
-        return new StudentProfile("Serah",1,"saragathoni888@gmail.com","Software Development");
+        return new StudentProfile("Buyahi",2,"buyahi@gmail.com","mobile dev");
     }
 
 
