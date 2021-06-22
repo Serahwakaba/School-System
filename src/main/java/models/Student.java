11aae5e3b386bdfd79b09c54cc62edea038bd910
Serah wakaba <serahwakaba9@gmail.com>
2021-06-22 10:47:28 +0300

    students model commit

--- src/main/java/models/Student.java
+++ src/main/java/models/Student.java
@@ -3,65 +3,8 @@ package models;
 import java.util.Objects;
 
 public class Student {
-    public int id;
-    public String name;
-    public int email;
-    public int admissionNumber;
-    public String units;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return id == student.id && email == student.email && admissionNumber == student.admissionNumber && Objects.equals(name, student.name) && Objects.equals(units, student.units);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email, admissionNumber, units);
-    }
 
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getEmail() {
-        return email;
-    }
-
-    public void setEmail(int email) {
-        this.email = email;
-    }
-
-    public int getAdmissionNumber() {
-        return admissionNumber;
-    }
-
-    public void setAdmissionNumber(int admissionNumber) {
-        this.admissionNumber = admissionNumber;
-    }
-
-    public String getUnits() {
-        return units;
-    }
-
-    public void setUnits(String units) {
-        this.units = units;
-    }
 
 
 }
--- src/main/java/models/Students.java
+++ src/main/java/models/Students.java
@@ -1,4 +1,67 @@
 package models;
 
+import java.util.Objects;
+
 public class Students {
+    public int id;
+    public String name;
+    public int admissionNumber;
+    public String units;
+    public int email;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmail() {
+        return email;
+    }
+
+    public void setEmail(int email) {
+        this.email = email;
+    }
+
+    public int getAdmissionNumber() {
+        return admissionNumber;
+    }
+
+    public void setAdmissionNumber(int admissionNumber) {
+        this.admissionNumber = admissionNumber;
+    }
+
+    public String getUnits() {
+        return units;
+    }
+
+    public void setUnits(String units) {
+        this.units = units;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Students students = (Students) o;
+        return id == students.id && email == students.email && admissionNumber == students.admissionNumber && Objects.equals(name, students.name) && Objects.equals(units, students.units);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, admissionNumber, units);
+    }
+
+
 }
