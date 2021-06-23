89fa1a95d14c84923f66ede200ad6a4b8d376b12
daniel-karigi <muchai.karigi@gmail.com>
2021-06-23 18:56:40 +0300

    update

--- src/main/java/Hostels.java
+++ src/main/java/Hostels.java
@@ -2,7 +2,7 @@ import org.sql2o.*;
 
 import java.util.List;
 
-public class Hostels{
+public class Hostels {
     private String username;
     private String firstName;
     private String studentId;
@@ -61,49 +61,50 @@ public class Hostels{
     }
 
     public void save() {
-        try(Connection con = Database.sql2o.open()) {
-           String sql = "INSERT INTO hostels (username, first_name, studentId,password,email) VALUES (:username, :first_name, :hostel_id, password,email,now())";
-           this.id = (int) con.createQuery(sql, true)
-                   .addParameter("studentId", this.studentId)
-           .addParameter("username", this.username)
-           .addParameter("password", this.password)
-           .addParameter("email", this.email)
-           .executeUpdate()
-           .getKey();
+        try (Connection con = Database.sql2o.open()) {
+            String sql = "INSERT INTO hostels (username, first_name, studentId,password,email) VALUES (:username, :first_name, :hostel_id, password,email,now())";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("studentId", this.studentId)
+                    .addParameter("username", this.username)
+                    .addParameter("password", this.password)
+                    .addParameter("email", this.email)
+                    .executeUpdate()
+                    .getKey();
         }
-     }
-     
-     public static List<Hostels> all() {
+    }
+
+    public static List<Hostels> all() {
         String sql = "select * from hostels";
-        try(Connection con = Database.sql2o.open()) {
-           return con.createQuery(sql)
-           .executeAndFetch(Hostels.class);
+        try (Connection con = Database.sql2o.open()) {
+            return con.createQuery(sql)
+                    .executeAndFetch(Hostels.class);
         }
-     }
-    public static Hostels findById(int id)
-    {
-        try (Connection conn =Database.sql2o.open())
-        {
+    }
+
+    public static Hostels findById(int id) {
+        try (Connection conn = Database.sql2o.open()) {
             String getById = "SELECT * FROM hostels WHERE id = :id";
-            Hostels hostel=conn.createQuery(getById)
+            Hostels hostel = conn.createQuery(getById)
                     .addParameter("id", id)
                     .throwOnMappingFailure(false)
                     .executeAndFetchFirst(Hostels.class);
             return hostel;
         }
     }
-    public void update(int hostelId, String name, String location)
-    {
-        try (Connection conn = Database.sql2o.open()){
+
+    public void update(int hostelId, String name, String location) {
+        try (Connection conn = Database.sql2o.open()) {
             String sql = "UPDATE hostels SET hostelid=:hostelId, location=:location,name=:name WHERE id=:id";
             conn.createQuery(sql)
                     .addParameter("hostelId", hostelId)
                     .addParameter("location", location)
                     .addParameter("name", name)
-                    .addParameter("id",this.id)
+                    .addParameter("id", this.id)
                     .throwOnMappingFailure(false)
                     .executeUpdate();
 
         }
     }
 
+
+}
